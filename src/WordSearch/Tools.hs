e4a3ca79f682d3b5d5ab1e72005a8f876c707ceb
Helen Chu <55115447+helen-chu@users.noreply.github.com>
2022-12-16 22:43:17 -0500

    use .txt input instead of hardcode

--- src/WordSearch/Tools.hs
+++ src/WordSearch/Tools.hs
@@ -1,7 +1,7 @@
 module WordSearch.Tools
     (
         parsePuzzle
-    ,	findWord
+    ,   findWord
     ) where
 
 
@@ -15,7 +15,7 @@ findSublistIndex :: Eq a => [a] -> [a] -> Maybe Int
 findSublistIndex xss xs = findIndex (isPrefixOf xss) $ tails xs
 
 mapIndex :: (a -> Int -> b) -> [a] -> [b]
-mapIndex f l = map (uncurry f) $ zip l [0..length l - 1]
+mapIndex f l = zipWith f l [0..length l - 1]
 
 findWord :: String -> [[Letter]] -> [Letter]
 findWord s p = onlyString s $ head $ filter (checkIn s) (rows p ++ cols p ++ dial p ++ diar p ++ dial' p ++ diar' p)
@@ -24,15 +24,15 @@ findWord s p = onlyString s $ head $ filter (checkIn s) (rows p ++ cols p ++ dia
           indexOfR s line = fromJust $ findSublistIndex s (lts line) `orElse` findSublistIndex (reverse s) (lts line)
           onlyString s line = take (length s) $ drop (indexOfR s line) $ line
           rows p = p
-          cols p = transpose p
           dial p = transpose $ mapIndex (flip drop) p
           dial' p = transpose $ mapIndex (flip drop) (reverse p)
           diar p = transpose $ mapIndex (flip drop) (map reverse p)
           diar' p = transpose $ mapIndex (flip drop) (map reverse (reverse p))
+          cols = transpose
 
 parsePuzzle :: String -> ([[Letter]], [String])
 parsePuzzle p = (parseLetters $ lines p, parseWords $ lines p)
     where dup f a = f a a
           parseLine chars y = mapIndex (\[c] x -> (c, x, y)) $ words chars
-          parseLetters = mapIndex parseLine . dup (take . (subtract 2) . length)
+          parseLetters = mapIndex parseLine . dup (take . subtract 2 . length)
           parseWords = words . last
