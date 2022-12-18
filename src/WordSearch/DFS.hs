0b26c2ee43413e396bb6f58bc54607595736ab42
Helen Chu <55115447+helen-chu@users.noreply.github.com>
2022-12-18 00:38:15 -0500

    cleanup sequential

--- src/WordSearch/DFS.hs
+++ src/WordSearch/DFS.hs
@@ -1,7 +1,7 @@
 module WordSearch.DFS
-    (
-        dfs
-    ) where
+  (
+    dfs
+  ) where
 
 import WordSearch.Trie(Trie, getTrie, empty)
 import WordSearch.Tools(GridMap, Coord)
@@ -10,18 +10,16 @@ import Data.Maybe ( fromMaybe )
 
 dfs :: GridMap -> Trie Char -> Coord -> [Coord] -> String -> [String]
 dfs p trie index@(row, col) visited word = 
-    case Data.Map.lookup index p of
-        Nothing -> []
-        Just c  | index `elem` visited || not (member c children)   -> []
-                | isWord                                            -> newWord : recurse
-                | otherwise                                         -> recurse
-                where
-                    (_, children) = getTrie trie
-                    (isWord, _) = getTrie child
-                    newWord = word++[c]
-                    child = Data.Maybe.fromMaybe empty (Data.Map.lookup c children)
-                    leftwords = dfs p child (row, col-1) (index:visited) newWord
-                    rightwords = dfs p child (row, col+1) (index:visited) newWord
-                    topwords = dfs p child (row-1, col) (index:visited) newWord
-                    bottomwords = dfs p child (row+1, col) (index:visited) newWord
-                    recurse = leftwords ++ rightwords ++ topwords ++ bottomwords
+  case Data.Map.lookup index p of
+    Nothing -> []
+    Just c  | index `elem` visited || not (member c children) -> []
+            | isWord                                          -> newWord : recurse
+            | otherwise                                       -> recurse
+            where
+              (_, children) = getTrie trie
+              (isWord, _)   = getTrie child
+              newWord       = word++[c]
+              child         = fromMaybe empty (Data.Map.lookup c children)
+              trav ind      = dfs p child ind (index:visited) newWord
+              recurse       = trav (row, col-1) ++ trav (row, col+1)
+                              ++ trav (row-1, col) ++ trav (row+1, col)
