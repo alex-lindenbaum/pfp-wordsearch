dea1f589545fa9bd8c6d2ca5e1e8fe6ee51ba9c5
Alexander Lindenbaum <alexmac@dyn-209-2-219-253.dyn.columbia.edu>
2022-12-20 19:09:34 -0500

    draft of quarter parallel main

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,11 @@
 {-# OPTIONS_GHC -Wall #-}
 module Main (main) where
 
-import Lib (parsePuzzle', mkTrie, dfs)
+import Lib (parsePuzzle, splitInQuarter, mkTrie, dfs)
+import Data.List(nub)
 import System.Environment(getArgs, getProgName)
 import System.Exit(die);
+import Control.Parallel.Strategies
 import qualified Data.Map as Map
 
 main :: IO()
@@ -15,20 +17,33 @@ main = do
       pn <- getProgName
       die $ "Usage: " ++ pn ++ " <filename>"
   puzzle <- readFile filename
-  let (p, w) = parsePuzzle' puzzle
+  let (p, w) = parsePuzzle puzzle
       trie = mkTrie w
-      f index _ wordList = wordList ++ dfs p trie index [] ""
-      output = Map.foldrWithKey f [] p
-  mapM_ putStrLn output
-  -- foldl (\pos word -> dfs word p ++ pos) [] w
-  --     positions = nub $ foldl (\pos word -> findWord word p ++ pos) [] w
-  -- mapM_ (\ row -> do
-  --          mapM_ (\ l@(c, _, _) ->
-  --                   if l `notElem` positions
-  --                   then putChar c
-  --                   else putStr ("\x1b[32m" ++ [c] ++ "\x1b[0m")
-  --                ) row
-  --          putStrLn ""
-  --       ) p
-  -- print (Map.lookup (1, 2) p)
+      -- f index _ wordList = wordList ++ dfs p trie index [] ""
+      -- output = Map.foldrWithKey f [] p
+      output = runEval $ do
+        let result = parMap rpar (\(index, _) -> dfs p trie index [] "") (Map.toList p)
+        -- let result = (withStrategy (parBuffer 100 r) . map (\(index, _) -> dfs p trie index [] "")) (Map.toList p)
 
+        -- parMap strat f = withStrategy (parList strat) . map f
+        _ <- rseq result
+        return result
+
+
+
+  let (p, w) = parsePuzzle puzzle
+      trie = mkTrie w
+      dfsWrapper (index, _) = dfs p trie index [] ""
+      (q1,q2,q3,q4) = splitInQuarter . Map.toList p
+      output = runEval $ do
+        q1Result <- rpar (dfsWrapper q1)
+        q2Result <- rpar (dfsWrapper q2)
+        q3Result <- rpar (dfsWrapper q3)
+        q4Result <- rpar (dfsWrapper q4)
+        _ <- rseq q1Result
+        _ <- rseq q2Result
+        _ <- rseq q3Result
+        _ <- rseq q4Result
+        return nub $ q1Result ++ q2Result ++ q3Result ++ q4Result
+
+  print output
