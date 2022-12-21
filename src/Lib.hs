7013acca973a673987dbeadc8596282f64f52e30
Alexander Lindenbaum <alexmac@dyn-209-2-219-253.dyn.columbia.edu>
2022-12-20 19:11:14 -0500

    src for quarter parallel

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,13 +1,13 @@
 module Lib
   (
-    parsePuzzle'
-  , findWord
+    parsePuzzle
+  , splitInQuarter
   , mkTrie
   , dfs
   , empty
   ) where
 
-import WordSearch.Tools(parsePuzzle', findWord)
+import WordSearch.Tools(parsePuzzle, splitInQuarter)
 import WordSearch.Trie(mkTrie, empty)
 import WordSearch.DFS(dfs)
 
--- src/WordSearch/Tools.hs
+++ src/WordSearch/Tools.hs
@@ -1,50 +1,38 @@
 module WordSearch.Tools
   (
-    parsePuzzle'
-  , findWord
+    parsePuzzle
   , GridMap
   , Coord
+  , splitInQuarter
   ) where
 
 
-import Data.List (transpose, findIndex, isInfixOf, isPrefixOf, tails, nub)
 import Data.Map (Map, insert, empty)
-import Data.Maybe (fromJust)
-import Data.Generics.Aliases (orElse)
 
-type Letter = (Char, Int, Int)
 type Coord = (Int, Int)
 type GridMap = Map Coord Char
 
-findSublistIndex :: Eq a => [a] -> [a] -> Maybe Int
-findSublistIndex xss xs = findIndex (isPrefixOf xss) $ tails xs
 
-mapIndex :: (a -> Int -> b) -> [a] -> [b]
-mapIndex f l = zipWith f l [0..length l - 1]
-
-findWord :: String -> [[Letter]] -> [Letter]
-findWord s p = onlyString s $ head $ filter (checkIn s) (rows p ++ cols p ++ dial p ++ diar p ++ dial' p ++ diar' p)
-    where lts = map (\(c, _, _) -> c)
-          checkIn s line = s `isInfixOf` lts line || reverse s `isInfixOf` lts line
-          indexOfR s line = fromJust $ findSublistIndex s (lts line) `orElse` findSublistIndex (reverse s) (lts line)
-          onlyString s line = take (length s) $ drop (indexOfR s line) $ line
-          rows p = p
-          dial p = transpose $ mapIndex (flip drop) p
-          dial' p = transpose $ mapIndex (flip drop) (reverse p)
-          diar p = transpose $ mapIndex (flip drop) (map reverse p)
-          diar' p = transpose $ mapIndex (flip drop) (map reverse (reverse p))
-          cols = transpose
-
-parsePuzzle :: String -> ([[Letter]], [String])
-parsePuzzle p = (parseLetters $ lines p, parseWords $ lines p)
-    where dup f a = f a a
-          parseLine chars y = mapIndex (\[c] x -> (c, x, y)) $ words chars
-          parseLetters = mapIndex parseLine . dup (take . subtract 2 . length)
-          parseWords = words . last
+splitInQuarter :: [a] -> ([a], [a], [a], [a])
+splitInQuarter l = (a,b,c,d)
+  where (left, right) = splitInHalf l
+        (a,b) = splitInHalf left
+        (c,d) = splitInHalf right
 
 
-parsePuzzle' :: String -> (GridMap, [String])
-parsePuzzle' p = (grid, wordList)
+splitInHalf :: [a] -> ([a], [a])
+splitInHalf l = splitInHalfHelper l [] [] 0
+
+
+splitInHalfHelper :: [a] -> [a] -> [a] -> Int -> ([a], [a])
+splitInHalfHelper [] l r _ = (l, r)
+splitInHalfHelper (x:xs) l r parity
+  | parity == 0   = splitInHalfHelper xs (x:l) r 1
+  | otherwise     = splitInHalfHelper xs l (x:r) 0
+
+
+parsePuzzle :: String -> (GridMap, [String])
+parsePuzzle p = (grid, wordList)
     where linesP = lines p
           grid = parseGrid gridStrings 0 empty
           gridStrings = take (length linesP - 2) linesP
